8e75a6de61726ceb3c980479ec9e549b5d675064
m_yehya <mhammad.yehya.97@gmail.com>
2025-03-17 22:35:00 +0100

    edit req

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import torch.nn as nn
 from torchvision import transforms
 from PIL import Image
 import io
-import os
 
 app = Flask(__name__)
 
@@ -76,5 +75,4 @@ def predict():
     return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Default to 5000 for local development
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
